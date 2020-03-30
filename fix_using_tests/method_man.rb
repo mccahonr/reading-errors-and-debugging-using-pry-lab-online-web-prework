d409122193e4ec3216d0388883ab3b25a7a794b5
Ryan McCahon <mccahonr@yahoo.com>
2020-03-30 20:48:33 +0000

    Automatically backed up by Learn

--- fix_using_tests/method_man.rb
+++ fix_using_tests/method_man.rb
@@ -2,9 +2,9 @@
 
 def start_game(player1, player2)
 
-  puts "Hello #{player1} & #{playar2}"
+  puts "Hello #{player1} & #{player2}"
 end
 
-def play_game(player2)
+def play_game(player1, player2)
   puts "#{player1} is better than #{player2}"
 end
