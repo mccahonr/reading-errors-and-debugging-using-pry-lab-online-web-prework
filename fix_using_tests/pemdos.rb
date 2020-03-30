3aa7fd68e24c8a66024e4db116ffc129f0f8a77f
Ryan McCahon <mccahonr@yahoo.com>
2020-03-30 21:23:38 +0000

    Done.

--- fix_using_tests/pemdos.rb
+++ fix_using_tests/pemdos.rb
@@ -1,8 +1,9 @@
-# don't forget to add: require 'pry'
+# don't forget to add: requi
+require "pry"
 
 def snake_it_up(string)
   if string[0] == "s"
-    10 * "s" + string
+    string.insert(0, "ssssssssss")
   else
     string
   end
